1d119afb8761d2eb5224751a70791315472cbc0f
kenmckaba <ken@mckaba.com>
2021-03-10 20:55:01 -0800

    experiment with another implementation

--- log-array.js
+++ log-array.js
@@ -9,18 +9,38 @@
 // }
 
 // KM: I assume the intent is to log the array, 1 value every 3 seconds
+console.log('start')
 
-const arr = [10, 12, 15, 21];
+if (true) {
+  const arr = [10, 12, 15, 21];
 
-const process = (index) => {
-  const f = () => {
-    console.log('Index: ' + index + ', element: ' + arr[index]);
-    if (index < arr.length - 1) {
-      process(index + 1);
-    }
+  async function doIt() {
+    return await new Promise((resolve) => {
+      const process = (index) => {
+        const f = () => {
+          console.log('Index: ' + index + ', element: ' + arr[index]);
+          if (index < arr.length - 1) {
+            process(index + 1);
+          } else {
+            resolve(true);
+          }
+        };
+        setTimeout(f, 3000);
+      };
+      process(0);
+    });
   };
 
-  setTimeout(() => f(), 3000);
-};
+  doIt();
 
-process(0);
+} else {
+
+  const arr = [10, 12, 15, 21];
+  for (var i = 0; i < arr.length; i++) {
+    (function( _i ) {
+      setTimeout(function() {
+        console.log('Index: ' + _i + ', element: ' + arr[_i]);
+      }, 5000);
+    })(i)
+  }
+}
